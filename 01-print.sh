7c30a3bd21d47192ee58c94170fd33e27f15784f
Perugu swathi <Peruguswathi11@gmail.com>
2021-04-20 13:21:16 +0530

    #1 - Adding sample.txt file as needed whenever we want

--- 01-print.sh
+++ 01-print.sh
@@ -14,3 +14,23 @@ echo bye
 echo -e "hai1/nbye2"
 
 echo -e "hai1\nbye2"
+
+# \n is used to print new line
+
+# some times we need colors  to be printed with the text
+
+## following colors are supported
+#colors      code
+----------------------------------
+# red          31
+# green        32
+# blue         33
+# yellow       34
+# magenta      35
+# cyan         36
+
+# syntax: echo -e "\e[COLmMESSAGE"
+
+echo -e "\e[31mwelocme to devops training"
+
+
